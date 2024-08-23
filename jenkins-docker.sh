425695ae6ed83f64a56897a4696482d7e378488c
Ashish.s.panicker <ashish.s.panicker@outlook.com>
2024-08-23 15:53:53 +0530

    Docker files

---
+++ jenkins-docker.sh
@@ -0,0 +1,37 @@
+# Create a network for jenkins
+sudo docker network create jenkins
+
+# Run the jenkins
+sudo docker run \
+  --name jenkins-docker \
+  --rm \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind \
+  --storage-driver overlay2
+
+  # Create the docker image for jenkins
+
+  sudo docker build -t myjenkins-blueocean:2.462.1-1 .
+
+  # Run the jenkins image
+
+  docker run \
+  --name jenkins-blueocean \
+  --restart=on-failure \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  myjenkins-blueocean:2.462.1-1
